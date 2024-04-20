f6ab9ddd1b7d813d5cc736554a2ee3b81c4a44da
vimukthimadushan94 <madushan.gangoda94@gmail.com>
2024-04-20 19:01:06 +0300

    make migrations companies and stations table

---
+++ database/migrations/2024_04_20_153332_create_companies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('parent_company_id')->nullable();
+            $table->foreign('parent_company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
