e16ba8dc4aa2e8255dbb1a11716fc3c1b030bf80
vimukthimadushan94 <madushan.gangoda94@gmail.com>
2024-04-21 11:57:41 +0300

    add company_id as a optional query param to the list

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -71,9 +71,10 @@ class StationController extends Controller
         $latitude = $request->input('latitude');
         $longitude = $request->input('longitude');
         $radius = $request->input('radius');
+        $companyId = $request->input('company_id');
 
         if(isset($latitude) && isset($longitude) && isset($radius)){
-            $groupedStations = $this->stationRepository->getStations($latitude,$longitude,$radius);
+            $groupedStations = $this->stationRepository->getStations($latitude,$longitude,$radius,$companyId);
             return response()->json($groupedStations);
         }else{
             return response()->json(['error' => 'Station not found. Please provide location and range attributes'], 404);
--- app/Repositories/StationRepository.php
+++ app/Repositories/StationRepository.php
@@ -2,20 +2,30 @@
 
 namespace App\Repositories;
 
+use App\Models\Company;
 use App\Models\Station;
 
 class StationRepository
 {
-    public function getStations($latitude,$longitude,$radius)
+    public function getStations($latitude,$longitude,$radius,$companyId = null)
     {
+
+        $childCompanyIds = $this->getChildCompanyIds($companyId);
+        $stationIds = Station::whereIn('company_id', $childCompanyIds)->pluck('id');
+
         $stations = Station::selectRaw("*,
                     ( 6371 * acos( cos( radians(?) ) * cos( radians( latitude ) )
                       * cos( radians( longitude ) - radians(?)
                       ) + sin( radians(?) ) * sin( radians( latitude ) ) )
                     ) AS distance", [$latitude, $longitude, $latitude])
             ->having('distance', '<', $radius)
-            ->orderBy('distance')
-            ->get();
+            ->orderBy('distance');
+
+        if($stationIds){
+            $stations = $stations->whereIn('id', $stationIds)->get();
+        }else{
+            $stations = $stations->get();
+        }
 
         $groupedStations = $stations->groupBy(function ($station) {
             return $station->latitude . '_' . $station->longitude;
@@ -42,5 +52,18 @@ class StationRepository
 
         return $groupedStations;
 
+    }
+
+    private function getChildCompanyIds($companyId, &$ids = [])
+    {
+        $ids[] = $companyId;
+
+        $childCompanies = Company::where('parent_company_id', $companyId)->get();
+
+        foreach ($childCompanies as $company) {
+            $this->getChildCompanyIds($company->id, $ids);
+        }
+
+        return $ids;
     }
 }
